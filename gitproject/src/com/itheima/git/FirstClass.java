ae99203a5c54b0dfa15e4d84d41231903fe8e0b3
LMH <1424661628@qq.com>
2019-08-23 23:09:03 +0800

    把本地工程提交到git

---
+++ gitproject/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,19 @@
+package com.itheima.git;
+
+/**
+ * Created by lvmen on 2019/8/23
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+        System.out.println("hello git ");
+    }
+
+
+}
